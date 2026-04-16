b9591bc127ec2d9c5dfb55034bbaa7674d8baa61
sakethvvv <150539077+sakethvvv@users.noreply.github.com>
2026-04-16 09:46:05 +0530

    Refactor App component to use React Router

--- App.tsx
+++ App.tsx
@@ -1,50 +1,31 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
-import { Analyzer } from './components/Analyzer';
 import { Footer } from './components/Footer';
-import { FeaturesSection, GuideSection, TipsSection, TeamSection, VisionSection, ContactSection, ProblemSection, HowItWorksSection } from './components/Sections';
+
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
+import TermsPage from './pages/TermsPage';
+import DisclaimerPage from './pages/DisclaimerPage';
+import FAQPage from './pages/FAQPage';
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <Navbar />
-      
-      <main className="flex-grow">
-        <section id="home" className="scroll-mt-32 border-b border-slate-100">
-          <Analyzer />
-        </section>
 
-        {/* Problem Narrative Section */}
-        <section id="problem" className="scroll-mt-24">
-          <ProblemSection />
-        </section>
-
-        <section id="features" className="scroll-mt-24">
-          <FeaturesSection />
-        </section>
-
-        <section id="tips" className="scroll-mt-24">
-          <TipsSection />
-        </section>
-
-        <section id="guide" className="scroll-mt-24">
-          <GuideSection />
-        </section>
-
-        {/* New How It Works + Buyer Safety Section */}
-        <section id="how-it-works" className="scroll-mt-24">
-          <HowItWorksSection />
-        </section>
-
-        <VisionSection />
-
-        <section id="team" className="scroll-mt-24">
-          <TeamSection />
-        </section>
-
-        <section id="contact" className="scroll-mt-24">
-          <ContactSection />
-        </section>
+      <main className="flex-grow pt-20 md:pt-24">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path="/terms-and-conditions" element={<TermsPage />} />
+          <Route path="/disclaimer" element={<DisclaimerPage />} />
+          <Route path="/faq" element={<FAQPage />} />
+        </Routes>
       </main>
 
       <Footer />
